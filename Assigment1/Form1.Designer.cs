356620de1a772d9b9dc76f97f5d3934fa367520a
ranay <ranay@RANASCOM>
2025-03-03 19:35:04 +0300

    Proje dosyası ekle.

---
+++ Assigment1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Assigment1
+{
+    partial class frmToDoList
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnMarkComplete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTotalTasks = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.LightBlue;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEdit.ForeColor = System.Drawing.Color.Black;
+            this.btnEdit.Location = new System.Drawing.Point(20, 300);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(140, 30);
+            this.btnEdit.TabIndex = 0;
+            this.btnEdit.Text = "Edit Task";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.Location = new System.Drawing.Point(180, 300);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 30);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnMarkComplete
+            // 
+            this.btnMarkComplete.BackColor = System.Drawing.Color.Orange;
+            this.btnMarkComplete.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMarkComplete.Location = new System.Drawing.Point(340, 300);
+            this.btnMarkComplete.Name = "btnMarkComplete";
+            this.btnMarkComplete.Size = new System.Drawing.Size(180, 30);
+            this.btnMarkComplete.TabIndex = 2;
+            this.btnMarkComplete.Text = "Mark Completed";
+            this.btnMarkComplete.UseVisualStyleBackColor = false;
+            this.btnMarkComplete.Click += new System.EventHandler(this.btnMarkComplete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(350, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 30);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Navy;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.Location = new System.Drawing.Point(125, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(161, 38);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "To-Do List";
+            // 
+            // lblTotalTasks
+            // 
+            this.lblTotalTasks.AutoSize = true;
+            this.lblTotalTasks.BackColor = System.Drawing.Color.DarkBlue;
+            this.lblTotalTasks.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotalTasks.Location = new System.Drawing.Point(20, 340);
+            this.lblTotalTasks.Name = "lblTotalTasks";
+            this.lblTotalTasks.Size = new System.Drawing.Size(161, 28);
+            this.lblTotalTasks.TabIndex = 5;
+            this.lblTotalTasks.Text = "Total  Tasks : 0";
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTask.Location = new System.Drawing.Point(20, 60);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(320, 34);
+            this.txtTask.TabIndex = 6;
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 28;
+            this.lstTasks.Location = new System.Drawing.Point(20, 100);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(450, 200);
+            this.lstTasks.TabIndex = 7;
+            // 
+            // frmToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(601, 403);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.lblTotalTasks);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMarkComplete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Name = "frmToDoList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To-Do List Application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmToDoList_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnMarkComplete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTotalTasks;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.ListBox lstTasks;
+    }
+}
+
---
+++ Assigment1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assigment1
+{
+    public partial class frmToDoList: Form
+    {
+        private const string FilePath = "tasks.txt";
+        public frmToDoList()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                lstTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+                UpdateTaskCount();
+
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if(lstTasks.SelectedIndex != -1)
+            {
+                string editedTask = txtTask.Text;
+                if (!string.IsNullOrWhiteSpace(editedTask))
+                {
+                    lstTasks.Items[lstTasks.SelectedIndex] = editedTask;
+                    txtTask.Clear();
+                }
+        }
+    }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                UpdateTaskCount();
+            }
+        }
+
+        private void btnMarkComplete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                string task = lstTasks.SelectedItem.ToString();
+                if (!task.StartsWith("✔ "))
+                {
+                    lstTasks.Items[lstTasks.SelectedIndex] = "✔ " + task;
+                }
+            }
+        }
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                txtTask.Text = lstTasks.SelectedItem.ToString().Replace("✔ ", "");
+            }
+        }
+        private void UpdateTaskCount()
+        {
+            lblTotalTasks.Text = "Total Tasks: " + lstTasks.Items.Count;
+        }
+        private void SaveTasks()
+        {
+            File.WriteAllLines(FilePath, lstTasks.Items.Cast<string>());
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(FilePath))
+            {
+                string[] tasks = File.ReadAllLines(FilePath);
+                lstTasks.Items.AddRange(tasks);
+                UpdateTaskCount();
+            }
+        }
+
+
+        private void frmToDoList_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasks();
+        }
+    }
+
+
+
+}
